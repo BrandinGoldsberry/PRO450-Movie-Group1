39acb65c68e3a7005957214f7a207df1b01b41a0
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2021-01-14 19:58:54 -0700

    package.json changes

--- stack-it-up/src/App.js
+++ stack-it-up/src/App.js
@@ -18,7 +18,7 @@ export default function App() {
           <Header />
           <Switch>
             <Route exact path="/">
-              {/* <LandingPage /> */}
+              <LandingPage />
             </Route>
             <Route path="/login">
               <LogIn />
