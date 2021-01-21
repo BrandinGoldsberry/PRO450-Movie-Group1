3202da029fbcc386339bd5e291dcce4d73c669cc
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2021-01-20 20:38:14 -0700

    Stars component docs

--- stack-it-up/src/PagePart/Stars.js
+++ stack-it-up/src/PagePart/Stars.js
@@ -5,19 +5,19 @@ import ReactStars from "react-rating-stars-component";
 const Stars = props => {
 
     // const movieId = props.movieId;
-    const edit = !!props.edit;
-    const rating = props.rating || 0;
-    const onChange = props.onChange || null;
+    const edit = !!props.edit;                  // Boolean
+    const rating = props.rating || 0;           // Float; rating == value
+    const onChange = props.onChange || null;    // Function
 
     return (
         <ReactStars
             key={Math.random()} //TODO this is terrible
             count={5}
             size={24}
-            isHalf={!edit}
+            isHalf={!edit} // Users can make reviews with whole numbers (edit), average total ratings use halves (no edit)
             edit={edit}
             value={rating}
-            onChange={onChange}
+            onChange={onChange} // Not used if edit == false
             emptyIcon={<i className="far fa-star"></i>}
             halfIcon={<i className="fa fa-star-half-alt"></i>}
             fullIcon={<i className="fa fa-star"></i>}
