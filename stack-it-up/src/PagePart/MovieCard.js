999c9aeb82b51282513637178cea5cc1e538c408
BrandinGoldsberry <bgoldsberry@student.neumont.edu>
2021-01-21 13:56:19 -0700

    Added review posting and editing

--- stack-it-up/src/PagePart/MovieCard.js
+++ stack-it-up/src/PagePart/MovieCard.js
@@ -7,14 +7,77 @@ import Axios from 'axios';
 
 const movieUrl = "https://image.tmdb.org/t/p/w200";
 
+const Review = props => {
+    const [isEdit, setIsEdit] = useState();
+    const [reviewText, setReviewText] = useState(props.reviewText);
+    const [rating, setRating] = useState(props.rating);
+
+    var curTextEntry = "";
+    var curRating = 0;
+
+    const postReview = () => {
+        // Submit review to API
+        var res = Axios.put('http://localhost:5001/reviews/update-review', {
+            reviewText: curTextEntry,
+            rating: curRating,
+            reviewId: props.reviewId
+        });
+        setReviewText(curTextEntry);
+        setRating(curRating);
+        setIsEdit(false);
+    }
+
+    const onNewText = (e) => {
+        curTextEntry = e.target.value;
+    }
+    const onNewRating = (e) => {
+        curRating = e;
+    }
+
+    return(
+        <div>
+            {
+                isEdit &&
+                <div>
+                    <Stars 
+                    edit={true}
+                    rating={rating}
+                    onChange={onNewRating}
+                    />
+                    <input placeholder={reviewText} onChange={onNewText} type="text"/>
+                    <button onClick={postReview}>Post Review</button>
+                </div>
+            } {
+                !isEdit &&
+                <div>
+                    <Stars 
+                    edit={false}
+                    rating={rating}
+                    />
+                    <p>"{reviewText}"</p>
+                    <p> - {props.reviewer}</p>
+                    {
+                        props.isOwned &&
+                        <button onClick={() => setIsEdit(true)}>Edit Review</button>
+                    }
+                </div>
+            }
+        </div>
+    )
+}
+
 const MovieCard = props => {
     const movie = props.movie;
     const [rating, setRating] = useState();
     const [reviewCount, setReviewCount] = useState(0);
     const [userReview, setUserReview] = useState();
     const [userReviews, setUserReviews] = useState();
+    const [showReviews, setShowReviews] = useState(false);
     
     const { userData, setUserData } = useContext(UserContext);
+
+    var curTextEntry = "";
+    var curRating = 0;
 
     const getRatings = async () => {
         let reviews = await Axios.get(`http://localhost:5001/reviews/reviews-by-movie?movieId=${movie.id}`);
@@ -38,7 +101,9 @@ const MovieCard = props => {
             reviews.map((review, i) => {
                 reviewList.push(
                     <Stars
-                        className={review.review || "User did not enter review"}
+                        className={review.review}
+                        data-username={review.username}
+                        data-id={review._id}
                         key={i}
                         edit={false}
                         rating={review.rating}
@@ -55,15 +120,33 @@ const MovieCard = props => {
         if (userData?.user) {
             let userReview = await Axios.get(`http://localhost:5001/reviews/user-reviewed-movie?movieId=${movie.id}&userId=${userData.user.id}`);
             if (userReview.data.reviews?.length > 0) {  // Allow user to change review
-                console.log(userReview.data.reviews[0]);
                 setUserReview(userReview.data.reviews[0]);
             }
         }
     }
 
+    const postReview = () => {
+        // Submit review to API
+        Axios.post('http://localhost:5001/reviews/post-review', {
+            email: userData.user.email,
+            reviewText: curTextEntry,
+            rating: curRating,
+            movieId: movie.id,
+            username: userData.user.username
+        });
+    }
+
+    const onNewText = (e) => {
+        curTextEntry = e.target.value;
+    }
+    const onNewRating = (e) => {
+        curRating = e;
+    }
+
     useEffect(() => {
         getRatings();
     }, [!rating, !reviewCount, !userReview]);
+
 
     return (
         <div className="movie-card">
@@ -73,17 +156,20 @@ const MovieCard = props => {
             <p>{movie.overview.slice(0, 65)}...</p>
             <div style={ {display: "flex", flexDirection: "row"} } >
                 <Stars
-                    edit={false}
+                    edit={userData.user != undefined}
                     rating={rating}
                 />
                 <p className="ml-2 mb-0" style={ {lineHeight: "32px"} }>({reviewCount || '0'} ratings)</p>
-                {userData?.user && userReview?.review ? <>
+                <button onClick={() => setShowReviews(!showReviews)}>{showReviews ? "Hide" : "Show"} Reviews</button>
+                {/* {userData?.user && userReview?.review ? <>
                     <p>Change your review ({userReview.review})</p>
                 </> : <>
                     <p>Make a review</p>
-                </>}
+                </>} */}
             </div>
             <div>
+            {
+                showReviews &&
                 <>
                     {
                     //So I have to iterate of a collection of components to get it to even render, 
@@ -98,20 +184,32 @@ const MovieCard = props => {
                     //Seriously who made this?
                     userReviews &&
                     userReviews.map((review) => {
-                        console.log(review);
                         return(
-                            <div>
-                                <Stars 
-                                edit={false}
-                                rating={review.props.rating.$numberDecimal}
-                                />
-                                <p>{review.props.className}</p>
-                            </div>
+                           <Review
+                            rating={review.props.rating.$numberDecimal}
+                            reviewText={review.props.className}
+                            reviewer={review.props["data-username"]}
+                            isOwned={userData.user?.username === review.props["data-username"]}
+                            reviewId={review.props["data-id"]}
+                           />
                         )
                     })
                     }
                 </>
+            }
             </div>
+            {
+                userData.user != undefined && showReviews &&
+                <div>
+                    <Stars 
+                    edit={true}
+                    rating={0}
+                    onChange={onNewRating}
+                    />
+                    <input onChange={onNewText} type="text"/>
+                    <button onClick={postReview}>Post Review</button>
+                </div>
+            }
         </div>
     );
 }
